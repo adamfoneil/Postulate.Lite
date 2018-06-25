16c00bc09a02f84d14008fe51b6487126e86ab5f
Adam O'Neil <adamosoftware@gmail.com>
2018-06-25 19:50:29 -0400

    fixed some access modifiers

--- Postulate.Lite.SqlServer/SqlServerProvider_Merge.cs
+++ Postulate.Lite.SqlServer/SqlServerProvider_Merge.cs
@@ -12,7 +12,7 @@ namespace Postulate.Lite.SqlServer
 	{
 		public override string CommentPrefix => "--";
 
-		protected override string CreateTableCommand(Type modelType)
+		public override string CreateTableCommand(Type modelType)
 		{
 			var columns = MappedColumns(modelType);
 			var pkColumns = GetPrimaryKeyColumns(modelType, columns, out bool identityIsPrimaryKey);
@@ -31,42 +31,47 @@ namespace Postulate.Lite.SqlServer
 				")";
 		}
 
-		protected override string AddForeignKeyCommand(PropertyInfo propertyInfo)
+		public override string AddForeignKeyCommand(PropertyInfo propertyInfo)
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override string AddColumnCommand(PropertyInfo propertyInfo)
+		public override string AddColumnCommand(PropertyInfo propertyInfo)
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override string AlterColumnCommand(PropertyInfo propertyInfo)
+		public override string AlterColumnCommand(PropertyInfo propertyInfo)
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override string DropColumnCommand(ColumnInfo columnInfo)
+		public override string DropColumnCommand(ColumnInfo columnInfo)
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override string DropForeignKeyCommand(ForeignKeyInfo columnInfo)
+		public override string DropForeignKeyCommand(ForeignKeyInfo columnInfo)
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override string DropTableCommand(TableInfo tableInfo)
+		public override string DropTableCommand(TableInfo tableInfo)
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override string DropPrimaryKeyCommand(Type modelType)
+		public override string DropPrimaryKeyCommand(Type modelType)
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override string AddPrimaryKeyCommand(Type modelType)
+		public override string AddPrimaryKeyCommand(Type modelType)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string AddForeignKeyCommand(ForeignKeyInfo foreignkeyInfo)
 		{
 			throw new NotImplementedException();
 		}
