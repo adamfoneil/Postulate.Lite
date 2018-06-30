9632cc70a54b9f5068db778020497e5d1cc331d0
Adam O'Neil <adamosoftware@gmail.com>
2018-06-29 23:44:45 -0400

    started on MySql

---
+++ Postulate.Lite.MySql/MySqlProvider_Crud.cs
@@ -0,0 +1,88 @@
+﻿using Postulate.Lite.Core;
+using Postulate.Lite.Core.Extensions;
+using Postulate.Lite.Core.Metadata;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+
+namespace Postulate.Lite.MySql
+{
+	public partial class MySqlProvider<TKey> : CommandProvider<TKey>
+	{
+		public MySqlProvider(Func<object, TKey> identityConverter, string identitySyntax) : base(identityConverter, identitySyntax)
+		{
+		}
+
+		protected override string ApplyDelimiter(string name)
+		{
+			return $"`{name}`";
+		}
+
+		protected override string DeleteCommand<T>()
+		{
+			return $"DELETE FROM {ApplyDelimiter(TableName(typeof(T)))} WHERE {ApplyDelimiter(typeof(T).GetIdentityName())}=@id";
+		}
+
+		protected override string FindCommand<T>(string whereClause)
+		{
+			var type = typeof(T);
+			var props = MappedColumns(type);
+			var columns = props.Select(pi => new ColumnInfo(pi));
+			return $"SELECT {string.Join(", ", columns.Select(col => ApplyDelimiter(col.ColumnName)))} FROM {ApplyDelimiter(TableName(typeof(T)))} WHERE {whereClause}";
+		}
+
+		protected override string InsertCommand<T>()
+		{
+			var columns = EditableColumns<T>(SaveAction.Insert);
+			string columnList = string.Join(", ", columns.Select(c => ApplyDelimiter(c.ColumnName)));
+			string valueList = string.Join(", ", columns.Select(c => $"@{c.PropertyName}"));
+			return $"INSERT INTO {ApplyDelimiter(TableName(typeof(T)))} ({columnList}) VALUES ({valueList}); SELECT LAST_INSERT_ID()";
+		}
+
+		protected override string SqlColumnSyntax(PropertyInfo propertyInfo, bool isIdentity)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Dictionary<Type, string> SupportedTypes(int length = 0, int precision = 0, int scale = 0)
+		{
+			return new Dictionary<Type, string>()
+			{
+				{ typeof(string), $"varchar({length})" },
+				{ typeof(int), "int" },
+				{ typeof(DateTime), "datetime" },
+				{ typeof(bool), "bit" },
+				{ typeof(decimal), $"decimal({scale}, {precision})" },
+				{ typeof(long), "bigint" },
+				{ typeof(short), "smallint" },
+				{ typeof(byte), "tinyint" },
+				{ typeof(TimeSpan), "time" },
+				{ typeof(double), "float" },
+				{ typeof(float), "float" },
+				{ typeof(char), "char(1)" },
+				{ typeof(byte[]), $"varbinary({length})" }
+			};
+		}
+
+		protected override string TableName(Type modelType)
+		{
+			string result = modelType.Name;
+
+			var tblAttr = modelType.GetCustomAttribute<TableAttribute>();
+			if (tblAttr != null && !string.IsNullOrEmpty(tblAttr.Name))
+			{
+				result = tblAttr.Name;
+			}
+
+			return result;
+		}
+
+		protected override string UpdateCommand<T>()
+		{
+			var columns = EditableColumns<T>(SaveAction.Update);
+			return $"UPDATE {ApplyDelimiter(TableName(typeof(T)))} SET {string.Join(", ", columns.Select(col => $"{ApplyDelimiter(col.ColumnName)}=@{col.PropertyName}"))} WHERE {ApplyDelimiter(typeof(T).GetIdentityName())}=@id";
+		}
+	}
+}
---
+++ Postulate.Lite.MySql/MySqlProvider_Merge.cs
@@ -0,0 +1,69 @@
+﻿using Postulate.Lite.Core;
+using Postulate.Lite.Core.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
+namespace Postulate.Lite.MySql
+{
+	public partial class MySqlProvider<TKey> : CommandProvider<TKey>
+	{
+		public override string CommentPrefix => "# ";
+
+		public override string CreateTableCommand(Type modelType)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string DropColumnCommand(ColumnInfo columnInfo)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string DropForeignKeyCommand(ForeignKeyInfo foreignKeyInfo)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string DropPrimaryKeyCommand(Type modelType)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string DropTableCommand(TableInfo tableInfo)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override IEnumerable<ForeignKeyInfo> GetDependentForeignKeys(IDbConnection connection, TableInfo tableInfo)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string AddColumnCommand(PropertyInfo propertyInfo)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string AddForeignKeyCommand(PropertyInfo propertyInfo)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string AddForeignKeyCommand(ForeignKeyInfo foreignkeyInfo)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string AddPrimaryKeyCommand(Type modelType)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string AlterColumnCommand(PropertyInfo propertyInfo)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
