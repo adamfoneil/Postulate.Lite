64400b991c4a0ef71c5e549199ace284e7a01fa1
Adam O'Neil <adamosoftware@gmail.com>
2018-09-06 08:06:09 -0400

    added Schema attribute #7

---
+++ Postulate.Lite.Core/Attributes/SchemaAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Postulate.Lite.Core.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class SchemaAttribute : Attribute
+	{
+		public SchemaAttribute(string schemaName)
+		{
+			Name = schemaName;
+		}
+
+		public string Name { get; }
+	}
+}
