b18bdceef054dfdb10cd37fa088a7b8ea65de43b
Adam O'Neil <adamosoftware@gmail.com>
2018-09-08 11:26:41 -0400

    version 1.1.5 added UniqueKey attribute

---
+++ Postulate.Lite.Core/Attributes/UniqueKeyAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Postulate.Lite.Core.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Class, AllowMultiple = false)]
+	public class UniqueKeyAttribute : Attribute
+	{
+		/// <summary>
+		/// Denotes a unique constraint on a single property
+		/// </summary>
+		public UniqueKeyAttribute()
+		{
+		}
+
+		/// <summary>
+		/// At the class level, describes a unique constraint with a set of columns
+		/// </summary>
+		public UniqueKeyAttribute(params string[] columnNames)
+		{
+			ColumnNames = columnNames;
+		}
+
+		public string[] ColumnNames { get; }
+	}
+}
