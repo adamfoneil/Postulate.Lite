fc66f9c0f9b1d2a082d8bc3f7cef3f7168f34c53
Adam O'Neil <adamosoftware@gmail.com>
2018-09-10 11:11:02 -0400

    DefaultExpression attribute

---
+++ Postulate.Lite.Core/Attributes/DefaultExpressionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Postulate.Lite.Core.Attributes
+{
+	/// <summary>
+	/// Use this when adding required columns to a table that already has data.
+	/// This specifies how to fill the new column so it won't be null
+	/// Used by SchemaSync.Library
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+	public class DefaultExpressionAttribute : Attribute
+	{
+		public DefaultExpressionAttribute(string expression)
+		{
+			Expression = expression;
+		}
+
+		public string Expression { get; }
+	}
+}
